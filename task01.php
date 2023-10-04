2da24339e792ea9f65e6b6938aca80fe977661d8
Bryan <bryan.ferrando@epitech.eu>
2023-10-04 08:43:32 +0100

    task01

--- task01.php
+++ task01.php
@@ -2,9 +2,9 @@
 
 function discover_type($age, $name, $gpa, $isStudent){
   if($isStudent){
-    echo "Hello my name is $name, I'm $age years old. I'm a student at Epitech with a GPA of $gpa.";
+    echo "Hello my name is $name, I'm $age years old. I'm a student at Epitech with a GPA of $gpa." . PHP_EOL;
   } else {
-    echo "Hello my name is $name, I'm $age years old. I'm not a student.";
+    echo "Hello my name is $name, I'm $age years old. I'm not a student." . PHP_EOL;
   }
 }
 
